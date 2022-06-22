9db465b2d01b90fae05d3252e773f2045a04432c
cj4ck <digital.k.jack@gmail.com>
2022-06-22 19:34:09 +0200

    init

---
+++ src/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 16:03:47 by cjackows          #+#    #+#             */
+/*   Updated: 2022/06/22 19:18:29 by cjackows         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "../libft/libft.h"
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+/**
+ * ! Remove later
+ */
+# include <stdio.h>
+
+int	ex(void);
+
+
+#endif
